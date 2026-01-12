9b24e80e9027d04dc0fde538e105458469963429
Sujahath <sujahathmhmd@gmail.com>
2026-01-12 21:48:44 +0530

    chore: calery and django working good

--- project/project/celery.py
+++ project/project/celery.py
@@ -1,13 +1,14 @@
 import os
+
 from celery import Celery
 
-# Set default Django settings for Celery
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.project.settings")
+# THIS is mandatory for Celery
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 app = Celery("project")
 
-# Load config from Django settings, using CELERY_ prefix
+# Load settings from Django
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Auto-discover tasks.py in all installed apps
+# Auto-discover tasks from installed apps
 app.autodiscover_tasks()
